2cd5aa850a8037d69c89e2d841cbf9a29c427a8e
Backlin <40052526+HLHotBlooded@users.noreply.github.com>
2022-11-01 15:16:28 +0800

    revers

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
-{
-  "name": "Salesforce JB Custom Activity",
-  "description": "Create a Journey Builder Custom Activity for making an http request.",
-  "repository": "https://github.com/timsim00/http-request",
-  "keywords": ["journey", "builder", "marketing", "cloud", "custom", "activity", "salesforce"],
-  "logo": "http://image.s6.exacttarget.com/lib/fe9112727360007b77/m/1/Salesforce_Marketing_Cloud.png",
-  "env": {
-  	"REQUEST_METHOD":{
-  		"required":false
-  	},
-  	"REQUEST_URL":{
-  		"required":false
-  	},
-  	"REQUEST_HEADERS":{
-  		"required":false
-  	},
-  	"REQUEST_BODY":{
-  		"required":false
-  	},
-  	"APP_NAME":null,
-  	"KEY":null,
-  	"CLIENT_ID":{
-  		"required":false
-  	},
-  	"CLIENT_SECRET":{
-  		"required":false
-  	},
-  	"ACTIVITY_NAME":{
-  		"required":false
-  	},
-  	"ACTIVITY_DESCRIPTION":{
-  		"required":false
-  	}
-  }
+'use strict';
+// Module Dependencies
+// -------------------
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var errorhandler = require('errorhandler');
+var http        = require('http');
+var path        = require('path');
+var request     = require('request');
+var routes      = require('./routes');
+var activity    = require('./routes/activity');
+
+var app = express();
+
+// Configure Express
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.raw({type: 'application/jwt'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//app.use(express.methodOverride());
+//app.use(express.favicon());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express in Development Mode
+if ('development' == app.get('env')) {
+  app.use(errorhandler());
 }
+
+// HubExchange Routes
+app.get('/', routes.index );
+app.post('/login', routes.login );
+app.post('/logout', routes.logout );
+
+// Custom Hello World Activity Routes
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
+
+//Toknization
+app.post('/sfmctokenization/resolve/', activity.resolveToken );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ var activity = require('./activity');
  * GET home page.
  */
 exports.index = function(req, res){
-    console.log( 'Get Home Page');
     if( !req.session.token ) {
         res.render( 'index', {
             title: 'Unauthenticated',
@@ -15,19 +14,17 @@ exports.index = function(req, res){
         });
     } else {
         res.render( 'index', {
-            title: 'Custom Journey Builder Activity',
+            title: 'Journey Builder Activity',
             results: activity.logExecuteData,
         });
     }
 };
 
 exports.login = function( req, res ) {
-      console.log( 'Get Home Page login');
     console.log( 'req.body: ', req.body );
     res.redirect( '/' );
 };
 
 exports.logout = function( req, res ) {
-      console.log( 'Get Home Page logout');
     req.session.token = '';
 };
